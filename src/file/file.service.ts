a5e15d08a881eaf82ecf4b43b2295cd957dd43a6
zaur bersanov <storeborz@mail.ru>
2025-01-15 21:46:54 +0300

    feat: обновлены комментарии в методе saveFiles для улучшения понимания кода

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -6,17 +6,18 @@ import { FileResponse } from './file.inteface'
 @Injectable()
 export class FileService {
 	async saveFiles(files: Express.Multer.File[], folder: string = 'products') {
-		const uploadedFolder = `${path}/uploads/${folder}`
+		const uploadedFolder = `${path}/uploads/${folder}` //  path - корневая папка
 
-		await ensureDir(uploadedFolder)
+		await ensureDir(uploadedFolder) // ensureDir - Если папки uploads/products ещё не существует, она будет создана.
 
 		const response: FileResponse[] = await Promise.all(
 			files.map(async file => {
 				const originalName = `${Date.now()}-${file.originalname}`
 
 				await writeFile(
+					// writeFile Записывает содержимое файла в указанную директорию и имя файл
 					`${uploadedFolder}/${originalName}`,
-					file.buffer
+					file.buffer // file.buffer Содержит бинарные данные файла, которые будут записаны на диск
 				)
 
 				return {
