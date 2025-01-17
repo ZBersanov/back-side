ed13c40ae918733801427dc25270811d555a5201
zaur bersanov <storeborz@mail.ru>
2025-01-17 03:46:40 +0300

    feat: добавлен функционал для управления отзывами, включая создание, получение и удаление отзывов

---
+++ src/review/review.controller.ts
@@ -0,0 +1,47 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { ReviewService } from './review.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { ReviewDto } from './dto/review.dto'
+import { CurrentUser } from 'src/user/decorators/user.decorator'
+
+@Controller('reviews')
+export class ReviewController {
+	constructor(private readonly reviewService: ReviewService) {}
+
+	@Auth()
+	@Get('by-storeId/:storeId')
+	async getByStoreId(@Param('storeId') storeId: string) {
+		return this.reviewService.getByStoreId(storeId)
+	}
+
+	@Auth()
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post(':productId/:storeId')
+	async create(
+		@Body() dto: ReviewDto,
+		@CurrentUser('id') userId: string,
+		@Param('productId') productId: string,
+		@Param('storeId') storeId: string
+	) {
+		return this.reviewService.create(dto, userId, productId, storeId)
+	}
+
+	@Auth()
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Delete(':id')
+	async delete(@Param('id') id: string, @CurrentUser('id') userId: string) {
+		return this.reviewService.delete(id, userId)
+	}
+}
